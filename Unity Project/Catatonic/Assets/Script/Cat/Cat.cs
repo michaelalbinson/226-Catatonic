7c161d5c83af1c246ad544a855078e2045fd6527
jeevancha <46986714+jeevancha@users.noreply.github.com>
2019-01-30 17:53:31 -0500

    Unity Project Added

---
+++ Unity Project/Catatonic/Assets/Script/Cat/Cat.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cat : MonoBehaviour
+{
+    private Rigidbody2D myBody;
+	private Animator anim;
+
+	public float speed, jumpForce;
+
+	private bool grounded;
+	private	bool asleep = false;
+	private float bounds;
+
+	private float catScale = 0.5f;
+	private float boundRight = 9.6f;
+	private float boundLeft = -9.6f;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+	void Movement(){
+		float h = Input.GetAxisRaw("Horizontal"); //get either left or right from arrow keys/keyboard
+
+		//actions availible when awake
+		if (!asleep){
+			//move left/right
+			if(h > 0 && (transform.position.x + catScale * 2.5) < boundRight){ //D/right arrow key
+				myBody.velocity = new Vector2(speed * h, myBody.velocity.y);
+
+				//face right when moving right
+				Vector3 scale = transform.localScale;
+				scale.x = catScale;
+				transform.localScale = scale;
+
+				//set run bool to true-> will set animation from idle to run
+				anim.SetBool("Run", true);
+
+
+			}
+			else if(h < 0 && (transform.position.x - catScale * 2.5) > boundLeft) { //A/left arrow key
+				myBody.velocity = new Vector2(speed * h, myBody.velocity.y);
+
+				//face left when moving left
+				Vector3 scale = transform.localScale;
+				scale.x = -catScale;
+				transform.localScale = scale;
+
+				anim.SetBool("Run", true);
+
+
+			}
+			else{ //idle
+				myBody.velocity = new Vector2(0, myBody.velocity.y);
+
+				//run to idle animation boolean
+				anim.SetBool("Run", false);
+			}
+			
+			//jump
+			if(Input.GetKey(KeyCode.Space)) {
+				if (grounded) {
+					grounded = false;
+
+					myBody.AddForce(new Vector2(transform.position.x, jumpForce));
+					anim.SetBool("Jump", true);
+				
+				}
+
+			}
+
+			//sleep
+			if(Input.GetKey("f")) {
+				if(grounded && h == 0){
+					anim.SetBool("Sleep", true);
+					asleep = true;
+				}
+			}
+		}
+
+		//actions availible when asleep
+		else if (asleep){
+			//wake up
+			if(Input.GetKey("q")) {
+				anim.SetBool("Sleep", false);
+				asleep = false;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D target){
+		if(target.gameObject.tag == "Ground") {
+			grounded = true;
+			anim.SetBool("Jump", false);
+		}
+		
+	}
+
+}
